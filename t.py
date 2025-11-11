b88fc21db4ed62e1babe776712d99c90e68f42f2
Cloud <94787440+MotorImagery@users.noreply.github.com>
2025-11-11 11:24:27 -0500

    Delete t.py

--- t.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# 每个受试者的样本数
-samples = [1626, 1760, 1794, 1440, 1760, 1760, 1308, 1760, 1760, 1760, 1760, 1600]
-
-# 计算平均值
-mean_samples = np.mean(samples)
-
-print("平均样本数:", mean_samples)
